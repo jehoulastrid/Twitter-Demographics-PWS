38bf631f72afaa2dab4e4f60dc3fbed03cec23dc
Jonathan Tonglet <49620076+jtonglet@users.noreply.github.com>
2022-05-03 21:17:22 +0200

    Update and rename M3_inference.py to M3_inference_test_set.py

--- Demographic_Inference/M3_inference_test_set.py
+++ Demographic_Inference/M3_inference_test_set.py
@@ -1,3 +1,7 @@
+"""
+The M3 model predicts the gender and age category of a Twitter user and whether or not the account belongs to an organization.
+"""
+
 #Import packages.
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -18,22 +22,19 @@ test_df_M3 = test_df[columns_to_keep]
 test_df_M3["id_str"] = test_df_M3.pop("user_id")
 test_df_M3["lang"] = ["nl" for i in range(len(test_df_M3))]
 test_df_M3["img_path"] = test_df_M3.pop('profile_image_url')
-
 os.mkdir("Demographic_Inference/jsonl_files/")
 file_to_write = ""
 for index in tqdm(range(len(test_df_M3))):  #user_df_M3.index
     test_df_M3.loc[index].to_json("Demographic_Inference/jsonl_files/row%s.json"%str(index))
     with open("Demographic_Inference/jsonl_files/row%s.json"%str(index)) as file_handle:
         file_content = file_handle.read()
-        file_to_write += file_content + "\n"
-        
+        file_to_write += file_content + "\n"   
 shutil.rmtree("Demographic_Inference/jsonl_files", ignore_errors=True)
-
 with open("Demographic_Inference/M3/M3_test_set.jsonl","w") as file_handle:
     file_handle.write(file_to_write)
     file_handle.close()
 
-#Load M3
+#Load M3 pre-trained model
 m3twitter= m3inference.M3Twitter(cache_dir="./Demographic_Inference/M3/profile_images_M3/")
 
 #Convert input to correct M3 format
